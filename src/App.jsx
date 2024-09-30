3dd61ecd72b7b61b279162501cb0e61326e3d3d9
Yhoan008 <yhoanmoreno8@gmail.com>
2024-09-30 18:52:37 -0500

    support

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="w-full h-[100vh] overflow-hidden relative ">
+      <div className=" w-[100vw] h-[100vw] bg-[#0CC9B0] rounded-full absolute -top-[100vh] -left-[50vw] " />
+    </div>
+  );
 }
